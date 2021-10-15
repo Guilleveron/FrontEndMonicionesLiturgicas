758e1feb05166edd6c0535cf6b4227a073ebab20
Guillermo Veron <guilleveron.gvf.gv@gmail.com>
2021-10-15 15:26:02 -0300

    gitignore y correcciones de node start

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var app = express();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var monicionesRouter = require('./routes/moniciones');
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,15 +30,10 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.set('port', process.env.PORT || 3000);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(app.get('port'), ()=> {
+    console.log(`Servidor Iniciado en puerto: ${app.get('port')}`);
 });
 
 module.exports = app;
